4fec278b7ae3d6be335b1cce80dcebe85288c58e
NicolasDenis57 <nicolas.denis.57@gmail.com>
2022-11-26 17:40:23 +0100

    new-addition

---
+++ js/script.js
@@ -0,0 +1,10 @@
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+    document.getElementById("container").style.opacity="0";
+  }
+  
+  function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+    document.getElementById("container").style.opacity="1";
+  }
+  
